89ee625bfe4e39f7f8f13a6de9bbcf1e64ba19bf
Oliko136 <olchick136@gmail.com>
2024-03-03 20:48:49 +0400

    Task 4 completed

--- src/4.ts
+++ src/4.ts
@@ -1,3 +1,57 @@
+class Key {
+    private signature: number;
+
+    constructor() {
+        this.signature = Math.random();
+    }
+
+    getSignature(): number {
+        return this.signature;
+    }
+}
+
+class Person {
+    private key: Key;
+
+    constructor(key: Key) {
+        this.key = key;
+    }
+
+    getKey(): Key {
+        return this.key;
+    }
+}
+
+abstract class House {
+    protected door: boolean = false;
+    protected key: Key;
+    protected tenants: Person[] = [];
+
+    comeIn(person: Person): void {
+        if (this.door) {
+            this.tenants.push(person);
+        } else {
+            console.log('The door is closed');
+        }
+    }
+
+    abstract openDoor(key: Key): void;
+}
+
+class MyHouse extends House {
+    constructor(key: Key) {
+        super()
+    }
+
+   openDoor(key: Key): void {
+       if (key.getSignature() === this.key.getSignature()) {
+           console.log('You may come in');
+       } else {
+           console.log('You have the wrong key');
+       }
+   }
+}
+
 const key = new Key();
 
 const house = new MyHouse(key);
